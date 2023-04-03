d68150a4080c2699cd22dcef35accd88420dee02
dedh121224 <dedh121224@gmail.com>
2023-04-03 17:44:13 +0800

    index.js加入註解說明

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
+// 設定模擬滑鼠自動點擊為 true
 let mockMouseAutoClick = true
 let root = undefined
 let rootStyles = undefined
 let activeColor = undefined
 let inactiveColor = undefined
 
+// 停止模擬滑鼠自動點擊
 const StopMockMouseAutoClick = () => {
     mockMouseAutoClick = false
 }
+
+// 開始模擬滑鼠自動點擊
 const StartMockMouseAutoClick = () => {
     if (!mockMouseAutoClick) {
         return
@@ -15,9 +19,13 @@ const StartMockMouseAutoClick = () => {
         StartMockMouseAutoClick()
     }, 1000);
 }
+
+// 設定 checkbox 狀態
 const SetCheckBox = (target, bool) => {
     target.prop('checked', bool)
 }
+
+// 取得已勾選的 checkbox 陣列
 const GetCheckedCheckBoxArray = () => {
     let checkBoxCheckeds = []
     if ($('#topCheckBox').prop('checked')) {
@@ -31,6 +39,8 @@ const GetCheckedCheckBoxArray = () => {
     }
     return checkBoxCheckeds
 }
+
+// 設定 checkbox 監聽事件
 const SetCheckBoxListener = () => {
     const input = document.querySelectorAll('.checkBox')
     for (let i = 0; i < input.length; i++) {
@@ -43,6 +53,8 @@ const SetCheckBoxListener = () => {
         })
     }
 }
+
+// 初始化
 const Init = () => {
     setTimeout(() => {
         $('body').mousedown(StopMockMouseAutoClick)
@@ -54,6 +66,9 @@ const Init = () => {
     }, 100)
 }
 const SetCheckedCheckBoxLessThanTwo = (number) => {
+
+// 設定勾選的 checkbox 小於等於 2
+const SetCheckedCheckBoxLessThanTwo= (number) => {
     let checkedCheckBoxArray = GetCheckedCheckBoxArray().filter(e => e[0].getAttribute('data-number') != number)
     if (checkedCheckBoxArray.length <= 1) {
         return
@@ -61,6 +76,8 @@ const SetCheckedCheckBoxLessThanTwo = (number) => {
     let randomInt = Math.floor(Math.random() * 2)
     SetCheckBox(checkedCheckBoxArray[randomInt], false)
 }
+
+// 設定子標題
 const SetSubTitle = () => {
     if ($('#topCheckBox').prop('checked')) {
         $(".switchSubTitleBotttom").hide()
@@ -84,10 +101,14 @@ const SetSubTitle = () => {
         SetTitleActiveColor($(".switchTitleRigth"), false)
     }
 }
+
+// 設定標題顏色
 const SetTitleActiveColor = (target, active) => {
     let color = active ? activeColor : inactiveColor
     target.css('color', color)
 }
+
+// 取得 CSS 樣式
 const GetCssStyle = () => {
     root = document.querySelector(':root')
     rootStyles = getComputedStyle(root)
@@ -95,4 +116,9 @@ const GetCssStyle = () => {
     inactiveColor = rootStyles.getPropertyValue('--color-title-imactive')
 }
 
+// 執行初始化
 Init()
+
+// 說明介紹：
+// 這份程式碼是一個簡單的網頁互動效果，當使用者勾選不同的 checkbox 時，會顯示不同的子標題，並改變標題的顏色。
+// 程式碼中還有一些其他的功能，例如模擬滑鼠自動點擊、設定 checkbox 狀態等等。
